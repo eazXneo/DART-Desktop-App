0747e3272d4ce16f83780c5b8979cbc444407625
Lea <s1953043@ed.ac.uk>
2024-08-21 15:46:16 +0200

    Finish testing output for 50 random images from GRAPE dataset. Use mocking for user input in original script.

--- tests/test_calculated_dart.py
+++ tests/test_calculated_dart.py
@@ -4,8 +4,11 @@ import glob
 import shutil
 import random
 import tempfile
+import pandas as pd
 
 from gui_interface.app import App
+from gui_interface.looping_over_images_in_folder_for_testing import run_original_dart_script
+from mock_user_input import *
 from pathlib import Path
 
 # def test_always_passes():
@@ -15,11 +18,14 @@ from pathlib import Path
 #     assert False
 
 IMAGE_N = 50
+GUI_RESULTS_FILENAME = "results_gui"
+OG_SCRIPT_RESULTS_FILENAME = "results_og"
+
 
 # @pytest.fixture
 # def teardown():
 #     print(os.path.join(os.getcwd(), "tests/data_subset"))
-    # [f.unlink() for f in Path("/path/to/folder").glob("*") if f.is_file()]
+# [f.unlink() for f in Path("/path/to/folder").glob("*") if f.is_file()]
 
 @pytest.fixture
 def setup_data():
@@ -47,21 +53,62 @@ def application(setup_data):
     app.menu.display_menu(import_dir_path)
     return app
 
-def test_50_random_images(application):
+
+@pytest.fixture
+def run_gui_dart(application):
     # if possible, hook into the app.run_dart function.
     # run dart from there
+
     app = application
     app.run_dart(
         ".jpg",
         False,
         str(os.path.join(os.getcwd(), "tests/test_results")),
         "",
-        "results_gui"
+        GUI_RESULTS_FILENAME
     )
 
+    path_to_results = str(os.path.join(os.getcwd(), "tests/test_results/", GUI_RESULTS_FILENAME))
+
+    return path_to_results
+
+
+@pytest.fixture
+def run_original_dart(setup_data):
     # run the original example script
 
+    set_keyboard_input([setup_data, "jpg", "no", str(os.path.join(os.getcwd(), "tests/test_results"))])
+
+    results = run_original_dart_script()  # TODO: if user input, then make a separate file for unittest stuff and throw into there, call here?
+
+    print('Writing results to file...')
+    with open(str(os.path.join(os.getcwd(), "tests/test_results/", (OG_SCRIPT_RESULTS_FILENAME + ".csv"))), 'w') as f:
+        f.write('image_path,image_name,FD\n')
+        for img_path, img_name, FD in results:
+            f.write(f'{img_path}, {img_name}, {FD}\n')
+
+    print('Created dart_inference_results.csv')
+
+    print('Done!')
+
+
+def test_compare_50_random_images(run_gui_dart, run_original_dart):
+    print("?")
+
     # open both csv files to compare them
+
     # make this a separate function potentially
+    # Read CSV files
+    df1_gui = pd.read_csv(str(os.path.join(os.getcwd(), "tests/test_results/", (GUI_RESULTS_FILENAME + ".csv"))))
+    df2_og = pd.read_csv(str(os.path.join(os.getcwd(), "tests/test_results/", (OG_SCRIPT_RESULTS_FILENAME + ".csv"))))
+
+    # Compare dataframes
+    diff = df1_gui.compare(df2_og)
+
+    # Print the differences
+    print("Differences between file1 and file2:")
+    print(diff)
+
+    assert len(diff) == 0
 
 # TODO: test parameters somehow via clicks?
